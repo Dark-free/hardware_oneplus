crdroidandroid/android_hardware_oneplus

hardware: Initial DeviceExtras 6.0 import!
- Now using a better resource based configuration method to allow
  universally any OnePlus device to ship this package and configure it
  by using overlays in their device specific tree.

Co-authored-by: Max Weninger <max.weninger@gmail.com>
Co-authored-by: oddlyspaced <oddlyspaced1@gmail.com>
Co-authored-by: TheScarastic <warabhishek@gmail.com>
Co-authored-by: LuK1337 <priv.luk@gmail.com>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: jhenrique09 <jhsv09@gmail.com>
Co-authored-by: AliHasan7671 <AHB7671@gmail.com>
Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
Co-authored-by: darkobas <darkobas@gmail.com>
Co-authored-by: Vachounet <vachounet@live.fr>
Co-authored-by: kldoc <fred.berbigier@gmail.com>
Co-authored-bt: Nauval Rizky <enuma.alrizky@gmail.com>
Co-authored-by: Christian Oder <myself5@carbonrom.org>
Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
Co-authored-by: SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
Co-authored-by: Michele Bono <bono.michele94@gmail.com>
Co-authored-by: LibXZR <xzr467706992@163.com>
Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Co-authored-by: Joey Huab <joey@evolution-x.org>
Co-authored-by: firebird11 <hbgassel@gmail.com>

DeviceExtras: Use our already available IOnePlusCameraProvider

- No need to import the same thing twice when it's already here.

Signed-off-by: AnierinB <anierin@evolution-x.org>
Signed-off-by: firebird11 <hbgassel@gmail.com>
Change-Id: Ib866f0facf049b647eee6164a46baa0fb546956d

DeviceExtras/src/org/lineageos/device/DeviceExtras/ModeSwitch/AutoHBMThresholdPreference.java
@@ -1,0 +1,51 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.lineageos.device.DeviceExtras;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import androidx.preference.PreferenceManager;
+
+public class AutoHBMThresholdPreference extends CustomSeekBarPreference {
+
+    public AutoHBMThresholdPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = context.getResources().getInteger(R.integer.auto_hbm_threshold_preference_interval);
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+        int[] mAllValues = context.getResources().getIntArray(R.array.auto_hbm_threshold_preference_array);
+        mMinValue = mAllValues[1];
+        mMaxValue = mAllValues[2];
+        mDefaultValueExists = true;
+        mDefaultValue = mAllValues[0];
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        mValue = Integer.parseInt(sharedPrefs.getString(DeviceExtras.KEY_AUTO_HBM_THRESHOLD, context.getString(R.string.auto_hbm_threshold_preference_default)));
+
+        setPersistent(false);
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        SharedPreferences.Editor prefChange = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        prefChange.putString(DeviceExtras.KEY_AUTO_HBM_THRESHOLD, String.valueOf(newValue)).commit();
+    }
+}

DeviceExtras/src/org/lineageos/device/DeviceExtras/ModeSwitch/HBMModeSwitch.java
@@ -1,0 +1,41 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.lineageos.device.DeviceExtras;
+
+import android.content.Context;
+
+public class HBMModeSwitch {
+
+    private static final int NODE = R.string.node_hbm_mode_switch;
+
+    public static String getFile(Context context) {
+        String file = context.getResources().getString(NODE);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(context), false);
+    }
+}

DeviceExtras/src/org/lineageos/device/DeviceExtras/ModeSwitch/VibratorStrengthPreference.java
@@ -1,0 +1,100 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2021-2022 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.com/licenses/>.
+*
+*/
+package org.lineageos.device.DeviceExtras;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Vibrator;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+import android.util.AttributeSet;
+
+public class VibratorStrengthPreference extends CustomSeekBarPreference {
+
+    private static int mDefVal;
+    private Vibrator mVibrator;
+
+    private static final int NODE_LEVEL = R.string.node_vibrator_strength_preference;
+    private static long testVibrationPattern[];
+
+    public VibratorStrengthPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = context.getResources().getInteger(R.integer.vibrator_strength_preference_interval);
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+
+        int[] mAllValues = context.getResources().getIntArray(R.array.vibrator_strength_preference_array);
+        mMinValue = mAllValues[1];
+        mMaxValue = mAllValues[2];
+        mDefaultValueExists = true;
+        mDefVal = mAllValues[0];
+        mDefaultValue = mDefVal;
+        mValue = Integer.parseInt(loadValue(context));
+
+        int[] tempVibrationPattern = context.getResources().getIntArray(R.array.test_vibration_pattern);
+        testVibrationPattern = new long[tempVibrationPattern.length];
+        for (int i = 0; i < tempVibrationPattern.length; i++) {
+            testVibrationPattern[i] = tempVibrationPattern[i];
+        }
+
+        setPersistent(false);
+
+        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    private static String getFile(Context context) {
+        String file = context.getString(NODE_LEVEL);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static void restore(Context context) {
+        if (!isSupported(context)) {
+            return;
+        }
+
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(DeviceExtras.KEY_VIBSTRENGTH, String.valueOf(mDefVal));
+        FileUtils.writeValue(getFile(context), storedValue);
+    }
+
+    public static String loadValue(Context context) {
+        return FileUtils.getFileValue(getFile(context), String.valueOf(mDefVal));
+    }
+
+    private void saveValue(String newValue) {
+        FileUtils.writeValue(getFile(getContext()), newValue);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putString(DeviceExtras.KEY_VIBSTRENGTH, newValue);
+        editor.apply();
+        mVibrator.vibrate(testVibrationPattern, -1);
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        saveValue(String.valueOf(newValue));
+    }
+}

DeviceExtras/src/org/lineageos/device/DeviceExtras/Slider/BrightnessController.java
@@ -1,0 +1,81 @@
+/*
+ * Copyright (C) 2018-2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.device.DeviceExtras.slider;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.Log;
+
+import org.lineageos.device.DeviceExtras.Constants;
+import org.lineageos.device.DeviceExtras.SliderControllerBase;
+
+public final class BrightnessController extends SliderControllerBase {
+
+    public static final int ID = 3;
+
+    private static final String TAG = "BrightnessController";
+
+    private static final int BRIGHTNESS_AUTO = 30;
+    private static final int BRIGHTNESS_BRIGHTEST = 31;
+    private static final int BRIGHTNESS_DARKEST = 32;
+
+    private static final int DARKEST = 1;
+    private static final int BRIGHTEST = 255;
+
+    public BrightnessController(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected int processAction(int action) {
+        Log.d(TAG, "slider action: " + action);
+        switch (action) {
+            case BRIGHTNESS_AUTO:
+                if (writeSettings(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                        Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC))
+                    return Constants.MODE_BRIGHTNESS_AUTO;
+                break;
+            case BRIGHTNESS_BRIGHTEST:
+                if (writeSettings(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                        Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL) &&
+                    writeSettings(Settings.System.SCREEN_BRIGHTNESS, BRIGHTEST))
+                    return Constants.MODE_BRIGHTNESS_BRIGHT;
+                break;
+            case BRIGHTNESS_DARKEST:
+                if (writeSettings(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                        Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL) &&
+                    writeSettings(Settings.System.SCREEN_BRIGHTNESS, DARKEST))
+                    return Constants.MODE_BRIGHTNESS_DARK;
+                break;
+            default:
+                return 0;
+        }
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+        writeSettings(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC);
+    }
+
+    private boolean writeSettings(String key, int value) {
+        return Settings.System.putIntForUser(mContext.getContentResolver(),
+                    key, value, UserHandle.USER_CURRENT);
+    }
+}

Dark-free/hardware_oneplus

DeviceExtras/src/org/evolution/device/DeviceExtras/ModeSwitch/AutoHBMThresholdPreference.java
@@ -1,0 +1,51 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.device.DeviceExtras;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import androidx.preference.PreferenceManager;
+
+public class AutoHBMThresholdPreference extends CustomSeekBarPreference {
+
+    public AutoHBMThresholdPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = context.getResources().getInteger(R.integer.auto_hbm_threshold_preference_interval);
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+        int[] mAllValues = context.getResources().getIntArray(R.array.auto_hbm_threshold_preference_array);
+        mMinValue = mAllValues[1];
+        mMaxValue = mAllValues[2];
+        mDefaultValueExists = true;
+        mDefaultValue = mAllValues[0];
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        mValue = Integer.parseInt(sharedPrefs.getString(DeviceExtras.KEY_AUTO_HBM_THRESHOLD, context.getString(R.string.auto_hbm_threshold_preference_default)));
+
+        setPersistent(false);
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        SharedPreferences.Editor prefChange = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        prefChange.putString(DeviceExtras.KEY_AUTO_HBM_THRESHOLD, String.valueOf(newValue)).commit();
+    }
+}

DeviceExtras/src/org/evolution/device/DeviceExtras/ModeSwitch/HBMModeSwitch.java
@@ -1,0 +1,41 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.evolution.device.DeviceExtras;
+
+import android.content.Context;
+
+public class HBMModeSwitch {
+
+    private static final int NODE = R.string.node_hbm_mode_switch;
+
+    public static String getFile(Context context) {
+        String file = context.getResources().getString(NODE);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(context), false);
+    }
+}

DeviceExtras/src/org/evolution/device/DeviceExtras/ModeSwitch/VibratorStrengthPreference.java
@@ -1,0 +1,100 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2021-2022 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.com/licenses/>.
+*
+*/
+package org.evolution.device.DeviceExtras;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Vibrator;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+import android.util.AttributeSet;
+
+public class VibratorStrengthPreference extends CustomSeekBarPreference {
+
+    private static int mDefVal;
+    private Vibrator mVibrator;
+
+    private static final int NODE_LEVEL = R.string.node_vibrator_strength_preference;
+    private static long testVibrationPattern[];
+
+    public VibratorStrengthPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = context.getResources().getInteger(R.integer.vibrator_strength_preference_interval);
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+
+        int[] mAllValues = context.getResources().getIntArray(R.array.vibrator_strength_preference_array);
+        mMinValue = mAllValues[1];
+        mMaxValue = mAllValues[2];
+        mDefaultValueExists = true;
+        mDefVal = mAllValues[0];
+        mDefaultValue = mDefVal;
+        mValue = Integer.parseInt(loadValue(context));
+
+        int[] tempVibrationPattern = context.getResources().getIntArray(R.array.test_vibration_pattern);
+        testVibrationPattern = new long[tempVibrationPattern.length];
+        for (int i = 0; i < tempVibrationPattern.length; i++) {
+            testVibrationPattern[i] = tempVibrationPattern[i];
+        }
+
+        setPersistent(false);
+
+        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    private static String getFile(Context context) {
+        String file = context.getString(NODE_LEVEL);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static void restore(Context context) {
+        if (!isSupported(context)) {
+            return;
+        }
+
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(DeviceExtras.KEY_VIBSTRENGTH, String.valueOf(mDefVal));
+        FileUtils.writeValue(getFile(context), storedValue);
+    }
+
+    public static String loadValue(Context context) {
+        return FileUtils.getFileValue(getFile(context), String.valueOf(mDefVal));
+    }
+
+    private void saveValue(String newValue) {
+        FileUtils.writeValue(getFile(getContext()), newValue);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putString(DeviceExtras.KEY_VIBSTRENGTH, newValue);
+        editor.apply();
+        mVibrator.vibrate(testVibrationPattern, -1);
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        saveValue(String.valueOf(newValue));
+    }
+}

DeviceExtras/src/org/evolution/device/DeviceExtras/Slider/BrightnessController.java
@@ -1,0 +1,81 @@
+/*
+ * Copyright (C) 2018-2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.evolution.device.DeviceExtras.slider;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.Log;
+
+import org.evolution.device.DeviceExtras.Constants;
+import org.evolution.device.DeviceExtras.SliderControllerBase;
+
+public final class BrightnessController extends SliderControllerBase {
+
+    public static final int ID = 3;
+
+    private static final String TAG = "BrightnessController";
+
+    private static final int BRIGHTNESS_AUTO = 30;
+    private static final int BRIGHTNESS_BRIGHTEST = 31;
+    private static final int BRIGHTNESS_DARKEST = 32;
+
+    private static final int DARKEST = 1;
+    private static final int BRIGHTEST = 255;
+
+    public BrightnessController(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected int processAction(int action) {
+        Log.d(TAG, "slider action: " + action);
+        switch (action) {
+            case BRIGHTNESS_AUTO:
+                if (writeSettings(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                        Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC))
+                    return Constants.MODE_BRIGHTNESS_AUTO;
+                break;
+            case BRIGHTNESS_BRIGHTEST:
+                if (writeSettings(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                        Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL) &&
+                    writeSettings(Settings.System.SCREEN_BRIGHTNESS, BRIGHTEST))
+                    return Constants.MODE_BRIGHTNESS_BRIGHT;
+                break;
+            case BRIGHTNESS_DARKEST:
+                if (writeSettings(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                        Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL) &&
+                    writeSettings(Settings.System.SCREEN_BRIGHTNESS, DARKEST))
+                    return Constants.MODE_BRIGHTNESS_DARK;
+                break;
+            default:
+                return 0;
+        }
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+        writeSettings(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC);
+    }
+
+    private boolean writeSettings(String key, int value) {
+        return Settings.System.putIntForUser(mContext.getContentResolver(),
+                    key, value, UserHandle.USER_CURRENT);
+    }
+}